1f79b344700f6d2526685f4cb1b960f3ca28938d
lukaszkomenda <loukas.komenda@gmail.com>
2022-11-13 19:16:41 +0100

    Add method:  helloName, makeAbba, makeTags

---
+++ src/main/java/org/example/HelloName/HelloName.java
@@ -0,0 +1,7 @@
+package org.example.HelloName;
+
+public class HelloName {
+    public String helloName(String name){
+        return "Hello " + name;
+    }
+}
---
+++ src/main/java/org/example/MakeTags/MakeTags.java
@@ -0,0 +1,7 @@
+package org.example.MakeTags;
+
+public class MakeTags {
+    public String makeTags(String tag, String word){
+        return "<" + tag + ">" + word + "</" + tag + ">";
+    }
+}
