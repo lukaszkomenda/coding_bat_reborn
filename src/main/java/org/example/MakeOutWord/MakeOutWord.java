87477a0e9c4e95d18a05e32271c190d211c8e5f8
lukaszkomenda <loukas.komenda@gmail.com>
2022-11-14 18:58:14 +0100

    Add method:  makeOutWord, extraEnd

---
+++ src/main/java/org/example/MakeOutWord/MakeOutWord.java
@@ -0,0 +1,8 @@
+package org.example.MakeOutWord;
+
+public class MakeOutWord {
+    public String makeOutWord(String out, String word){
+        return out.substring(0,2) + word + out.substring(2,4);
+
+    }
+}
